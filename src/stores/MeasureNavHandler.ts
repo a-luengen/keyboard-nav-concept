55536f7e8666ed9ab5b8d9dab2357f27ecd67f45
Alexander Lüngen <alex.luengen@klangio.com>
2025-07-25 18:32:50 +0200

    fix(MeasureNavHandler): correctly calculate the new measure index based on dynamic row count and measure length

--- src/stores/MeasureNavHandler.ts
+++ src/stores/MeasureNavHandler.ts
@@ -10,37 +10,49 @@ export default class MeasureNavHandler implements INavigationHandler {
   handleNavUp = () => {
     const [curMeasureIdx] = this.rootStore.curSelectionPos;
 
-    let newMeasureIdx =
-      curMeasureIdx < this.rootStore.MEASURES_PER_ROW
-        ? this.rootStore.measures.length - (this.rootStore.MEASURES_PER_ROW - 2)
-        : curMeasureIdx - this.rootStore.MEASURES_PER_ROW;
+    let newMeasureIdx = 0;
+    if (curMeasureIdx >= this.rootStore.MEASURES_PER_ROW) {
+      newMeasureIdx = curMeasureIdx - this.rootStore.MEASURES_PER_ROW;
+    } else {
+      const lastFullRowIdx =
+        Math.floor(
+          this.rootStore.measures.length / this.rootStore.MEASURES_PER_ROW
+        ) - 1;
+      const maxRowIdxInLastRow =
+        (this.rootStore.measures.length % this.rootStore.MEASURES_PER_ROW) - 1;
+      if (maxRowIdxInLastRow >= curMeasureIdx) {
+        // we can use the last row to navigate
+        newMeasureIdx =
+          (lastFullRowIdx + 1) * this.rootStore.MEASURES_PER_ROW +
+          curMeasureIdx;
+      } else {
+        // we need to jump in the second-last row
+        newMeasureIdx =
+          lastFullRowIdx * this.rootStore.MEASURES_PER_ROW + curMeasureIdx;
+      }
+    }
 
     this.rootStore.curSelectionPos = [newMeasureIdx];
-    this.rootStore.measures[curMeasureIdx].isActive = false;
-    this.rootStore.measures[newMeasureIdx].isActive = true;
+    this.switchActive(curMeasureIdx, newMeasureIdx);
   };
 
   handleNavDown = () => {
     const { curSelectionPos, measures, MEASURES_PER_ROW } = this.rootStore;
     const [curMeasureIdx] = curSelectionPos;
-    console.log('Cur Measure', curMeasureIdx);
     let newMeasureIdx = curMeasureIdx + MEASURES_PER_ROW;
     if (newMeasureIdx > measures.length - 1)
       newMeasureIdx = newMeasureIdx % MEASURES_PER_ROW;
     this.rootStore.curSelectionPos = [newMeasureIdx];
-    console.log('New Measure', newMeasureIdx);
-    console.log('Old Measure', curMeasureIdx);
-    measures[curMeasureIdx].isActive = false;
-    measures[newMeasureIdx].isActive = true;
+    this.switchActive(curMeasureIdx, newMeasureIdx);
   };
 
   handleNavLeft = () => {
     const [curMeasureIdx] = this.rootStore.curSelectionPos;
     const newMeasureIdx = Math.max(curMeasureIdx - 1, 0);
     this.rootStore.curSelectionPos = [newMeasureIdx];
-    this.rootStore.measures[curMeasureIdx].isActive = false;
-    this.rootStore.measures[newMeasureIdx].isActive = true;
+    this.switchActive(curMeasureIdx, newMeasureIdx);
   };
+
   handleNavRight = () => {
     const [curMeasureIdx] = this.rootStore.curSelectionPos;
     const newMeasureIdx = Math.min(
@@ -48,7 +60,12 @@ export default class MeasureNavHandler implements INavigationHandler {
       this.rootStore.measures.length - 1
     );
     this.rootStore.curSelectionPos = [newMeasureIdx];
-    this.rootStore.measures[curMeasureIdx].isActive = false;
-    this.rootStore.measures[newMeasureIdx].isActive = true;
+    this.switchActive(curMeasureIdx, newMeasureIdx);
   };
+
+  private switchActive(curMeasureIdx: number, newMeasureIdx: number) {
+    const { measures } = this.rootStore;
+    measures[curMeasureIdx].isActive = false;
+    measures[newMeasureIdx].isActive = true;
+  }
 }
