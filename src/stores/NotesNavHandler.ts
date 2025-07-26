036b0d3b95cb4a9283318d16a015bdf1743857ea
Alexander Lüngen <alex.luengen@klangio.com>
2025-07-26 17:54:19 +0200

    refactor(NotesNavHandler): extracted code to switch between active notes and measures

--- src/stores/NotesNavHandler.ts
+++ src/stores/NotesNavHandler.ts
@@ -26,10 +26,7 @@ export default class NotesNavHandler implements INavigationHandler {
     console.log('new Pos: ', newMeasureIdx, newNoteIdx);
 
     this.rootStore.curSelectionPos = [newMeasureIdx, newNoteIdx];
-    measures[mIdx].isActive = false;
-    measures[mIdx].notes[nIdx].isActive = false;
-    measures[newMeasureIdx].isActive = false;
-    measures[newMeasureIdx].notes[newNoteIdx].isActive = true;
+    this.switchActive([mIdx, nIdx, newMeasureIdx, newNoteIdx]);
   };
   handleNavDown = () => {
     const { curSelectionPos, measures, MEASURES_PER_ROW } = this.rootStore;
@@ -42,10 +39,7 @@ export default class NotesNavHandler implements INavigationHandler {
     const newNoteIdx = 0;
 
     this.rootStore.curSelectionPos = [newMeasureIdx, newNoteIdx];
-    measures[mIdx].isActive = false;
-    measures[mIdx].notes[nIdx].isActive = false;
-    measures[newMeasureIdx].isActive = false;
-    measures[newMeasureIdx].notes[newNoteIdx].isActive = true;
+    this.switchActive([mIdx, nIdx, newMeasureIdx, newNoteIdx]);
   };
   handleNavLeft = () => {
     const { curSelectionPos, measures } = this.rootStore;
@@ -54,8 +48,7 @@ export default class NotesNavHandler implements INavigationHandler {
     let newNoteIdx = nIdx - 1;
     if (newNoteIdx < 0) newNoteIdx = curMeasure.notes.length - 1;
     this.rootStore.curSelectionPos = [mIdx, newNoteIdx];
-    this.rootStore.measures[mIdx].notes[nIdx].isActive = false;
-    this.rootStore.measures[mIdx].notes[newNoteIdx].isActive = true;
+    this.switchActive([mIdx, nIdx, mIdx, newNoteIdx]);
   };
   handleNavRight = () => {
     const { curSelectionPos, measures } = this.rootStore;
@@ -63,7 +56,15 @@ export default class NotesNavHandler implements INavigationHandler {
     const [mIdx, nIdx] = curSelectionPos;
     const newNoteIdx = (nIdx + 1) % curMeasure.notes.length;
     this.rootStore.curSelectionPos = [mIdx, newNoteIdx];
-    this.rootStore.measures[mIdx].notes[nIdx].isActive = false;
-    this.rootStore.measures[mIdx].notes[newNoteIdx].isActive = true;
+    this.switchActive([mIdx, nIdx, mIdx, newNoteIdx]);
   };
+
+  private switchActive(vals: [number, number, number, number]) {
+    const [curMeasureIdx, curNoteIdx, newMeasureIdx, newNoteIdx] = vals;
+    const { measures } = this.rootStore;
+    measures[curMeasureIdx].isActive = false;
+    measures[curMeasureIdx].notes[curNoteIdx].isActive = false;
+    measures[newMeasureIdx].isActive = false;
+    measures[newMeasureIdx].notes[newNoteIdx].isActive = true;
+  }
 }
